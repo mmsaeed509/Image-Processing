11dedda58bc8a4d78949f4443f7822b55acf3792
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-12-13 08:14:06 +0200

    updated src code

--- Labs/Lab-Segmentation/src-code/main.py
+++ Labs/Lab-Segmentation/src-code/main.py
@@ -22,7 +22,52 @@ for ax in axes:
 plt.tight_layout()
 plt.show()
 
-# --------------- line detection --------------- #
+# --------------- line detection local processing --------------- #
+
+# define the vertical filter
+vertical_filter = [[-1, -2, -1], [0, 0, 0], [1, 2, 1]]
+
+# define the horizontal filter
+horizontal_filter = [[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]
+
+# read in the image
+img = plt.imread('img/testLines.jpg')
+
+# get the dimensions of the image
+n, m, d = img.shape
+
+# initialize the edges image
+edges_img = img.copy()
+
+# loop over all pixels in the image
+for row in range(3, n - 2):
+    for col in range(3, m - 2):
+        # create little local 3x3 box
+        local_pixels = img[row - 1:row + 2, col - 1:col + 2, 0]
+
+        # apply the vertical filter
+        vertical_transformed_pixels = vertical_filter * local_pixels
+        # remap the vertical score
+        vertical_score = vertical_transformed_pixels.sum() / 4
+
+        # apply the horizontal filter
+        horizontal_transformed_pixels = horizontal_filter * local_pixels
+        # remap the horizontal score
+        horizontal_score = horizontal_transformed_pixels.sum() / 4
+
+        # combine the horizontal and vertical scores into a total edge score
+        edge_score = (vertical_score ** 2 + horizontal_score ** 2) ** .5
+
+        # insert this edge score into the edges image
+        edges_img[row, col] = [edge_score] * 3
+
+# write new image #
+cv2.imwrite("output-img/local processing line .jpg", edges_img)
+
+cv2.imshow("local processing line", edges_img)
+cv2.waitKey(1000)
+
+# --------------- line detection Hough Transform --------------- #
 
 Image_2 = "img/testLines.jpg"
 # Loads an image
